374d4f4cecfea012c55f281a37f923ac1dfc3cfd
Thao Bear <thaobear.le@gmail.com>
2020-10-30 09:41:43 +0700

    update and delete experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -150,4 +150,73 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
+// @route   PUT api/profile/experience
+// @desc    update experience profile
+// @access  Private
+router.put(
+  '/experience',
+  auth,
+  [
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty(),
+  ],
+  validationParams,
+  async (req, res) => {
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newExp = { title, company, location, from, to, current, description };
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      if (err.kind == 'ObjectId')
+        return res.status(400).json({ msg: 'Profile not found' });
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    if (profile) {
+      const removeIndex = profile.experience
+        .map((item) => item.id)
+        .indexOf(req.params.exp_id);
+
+      profile.experience.splice(removeIndex, 1);
+
+      await profile.save();
+
+      res.json(profile);
+    }
+    res.status(400).json({ msg: 'Profile not found' });
+  } catch (err) {
+    console.error(err.message);
+    // if (err.kind == 'ObjectId')
+    //   return res.status(400).json({ msg: 'Profile not found' });
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
+//TODO: delete user => cap lai user -> van qua duojc auth
